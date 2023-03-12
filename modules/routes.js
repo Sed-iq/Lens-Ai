bf34b1ce587601849888521286eac0da99094a7a
sediq <you@example.com>
2023-03-12 13:27:22 +0100

    Added loading mechanisms

--- modules/routes.js
+++ modules/routes.js
@@ -4,7 +4,9 @@ const dontenv = require("dotenv").config();
 const generate = require("./generate");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.post("/generate", generate);
+app.post("/generate", (req, res) => {
+  res.json("3343sfsfvvsv333453343");
+});
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
